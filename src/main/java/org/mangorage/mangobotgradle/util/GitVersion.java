6fc7a8f736863836885f10a1162c8cba30cafd1a
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2025-02-26 05:00:41 -0800

    Updated to use JGit now :)

--- src/main/java/org/mangorage/mangobotgradle/util/GitVersion.java
+++ src/main/java/org/mangorage/mangobotgradle/util/GitVersion.java
@@ -1,28 +1,48 @@
 package org.mangorage.mangobotgradle.util;
 
-import java.io.BufferedReader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public record GitVersion(String tag, String commits, String commit) {
     private static final GitVersion UNKNOWN = new GitVersion("0.0", "9999", "unknown");
 
     public static GitVersion getGitVersion() {
         try {
-            var process = Runtime.getRuntime().exec("git describe --long --tags");
-            try (var isr = new InputStreamReader(process.getInputStream())) {
-                try (var br = new BufferedReader(isr)) {
-                    var result = br.readLine().split("-");
-                    return new GitVersion(
-                            result[0],
-                            result[1],
-                            result[2]
-                    );
+            FileRepositoryBuilder builder = new FileRepositoryBuilder();
+            try (Repository repository = builder.setGitDir(new File(".git")).readEnvironment().findGitDir().build()) {
+                try (RevWalk walk = new RevWalk(repository)) {
+                    RevCommit headCommit = walk.parseCommit(repository.resolve("HEAD"));
+                    String commitHash = headCommit.getName().substring(0, 7);
+
+                    // Find the latest tag
+                    String tag = repository.getRefDatabase().getRefsByPrefix("refs/tags/")
+                            .stream()
+                            .map(ref -> ref.getName().substring("refs/tags/".length()))
+                            .reduce((first, second) -> second) // Get the latest tag
+                            .orElse("");
+
+                    if (tag.isBlank())
+                        return UNKNOWN;
+
+                    // Count commits since the tag
+                    int commitCount = 0;
+                    walk.markStart(headCommit);
+                    for (RevCommit commit : walk) {
+                        commitCount++;
+                    }
+
+                    return new GitVersion(tag, String.valueOf(commitCount), commitHash);
                 }
             }
         } catch (IOException ignored) {}
         return UNKNOWN;
     }
+
 
     public boolean isUnknown() {
         return this == UNKNOWN;
